6d6fd02b98e9bf7ec23d23b94f81b3f43943fef0
KIMBOH LOVETTE <kimbohlovette@gmail.com>
2023-03-04 10:56:50 +0100

    Added todo reducers to the todoSlice

--- src/features/todo/todoSlice.ts
+++ src/features/todo/todoSlice.ts
@@ -11,7 +11,20 @@ const initialState: InitialState = {
 export const todoSlice = createSlice({
   name: 'todo',
   initialState,
-  reducers: {},
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos = [...state.todos, action.payload];
+    },
+    toggleTodoState: (state, action) => {
+      const id = action.payload;
+      const todo = state.todos.filter(t => t.id === id)[0];
+      todo.done = !todo.done;
+      state.todos = [...state.todos.filter(t => t.id !== id), todo];
+    },
+    dropTodo: (state, action) => {
+      state.todos = [...state.todos.filter(todo => todo.id !== action.payload)];
+    },
+  },
 });
 
 export default todoSlice.reducer;
