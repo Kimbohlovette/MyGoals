e333e4936d2eec7541e638465bcdd0c8322a61ba
KIMBOH LOVETTE <kimbohlovette@gmail.com>
2023-03-07 23:28:44 +0100

    Navigation working

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,44 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the Redux TypeScript template
- * https://github.com/rahsheen/react-native-template-redux-typescript
- *
  * @format
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  View,
-} from 'react-native';
-import MIcon from 'react-native-vector-icons/MaterialIcons';
-import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-import Goals from './components/Goals';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react';
+import Home from './pages/Home';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
 
-      <SafeAreaView>
-        <View style={styles.topNavbar}>
-          <MIcon name="chevron-left" size={30} />
-          <MCIcon name="dots-horizontal" size={30} />
-        </View>
-        <ScrollView style={styles.appContainer}>
-          <Goals />
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    // <SafeAreaView>
+    //   <ScrollView style={styles.appContainer}>
+    //     <Goals />
+    //   </ScrollView>
+    // </SafeAreaView>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({
-  topNavbar: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 8,
-    paddingVertical: 24,
-    width: '100%',
-  },
-  appContainer: {
-    paddingHorizontal: 8,
-    backgroundColor: 'white',
-  },
-});
+// const styles = StyleSheet.create({
+//   topNavbar: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//     paddingHorizontal: 8,
+//     paddingVertical: 24,
+//     width: '100%',
+//   },
+//   appContainer: {
+//     paddingHorizontal: 8,
+//     backgroundColor: 'white',
+//   },
+// });
